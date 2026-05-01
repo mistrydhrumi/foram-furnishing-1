1b48eb3a163b82eb4357ea6c38c280a7053dc7cd
mistrydhrumi <mistrydhrumi9134@gmail.com>
2026-05-01 11:29:29 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ import cors from 'cors'
 import wishlistRoutes from "./routes/wishlistRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
 import consultationRoutes from "./routes/consultationRoutes.js";
-
+import path from "path";
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'http://localhost:5176'
+        'http://localhost:5174'
     ],
     credentials: true
 }))
@@ -31,6 +31,11 @@ app.use('/api/v1/orders', orderRoute)
 app.use("/api/v1/wishlist", wishlistRoutes);
 app.use("/api/v1/contact", contactRoutes);
 app.use("/api/v1/consultation", consultationRoutes);
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 
 app.listen(PORT,()=>{
